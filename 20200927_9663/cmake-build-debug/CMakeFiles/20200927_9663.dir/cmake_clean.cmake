007cd1ad80dd82ff3c1929454d2386cdab827c6e
taehui.lee <taehui0118@gmail.com>
2020-09-27 17:58:02 +0900

    20200927 boj commit2

---
+++ 20200927_9663/cmake-build-debug/CMakeFiles/20200927_9663.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20200927_9663.exe"
+  "20200927_9663.exe.manifest"
+  "20200927_9663.pdb"
+  "CMakeFiles/20200927_9663.dir/main.cpp.obj"
+  "lib20200927_9663.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20200927_9663.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
