45aaa50edf3abdafa5fdc095d9b3cc3c10bdf1c3
taehui.lee <taehui0118@gmail.com>
2020-10-04 12:09:09 +0900

    20201004 boj commit

---
+++ 20201004_16928/cmake-build-debug/CMakeFiles/20201004_16928.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20201004_16928.exe"
+  "20201004_16928.exe.manifest"
+  "20201004_16928.pdb"
+  "CMakeFiles/20201004_16928.dir/main.cpp.obj"
+  "lib20201004_16928.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20201004_16928.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 20201004_2048/cmake-build-debug/CMakeFiles/20201004_2048.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "20201004_2048.exe"
+  "20201004_2048.exe.manifest"
+  "20201004_2048.pdb"
+  "CMakeFiles/20201004_2048.dir/main.cpp.obj"
+  "lib20201004_2048.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20201004_2048.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
